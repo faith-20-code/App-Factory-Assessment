61d197680a6874724bf044707e5fe98d1efa1e57
Faith <varaidzondu@gmail.com>
2026-05-22 13:11:59 +0300

    l tried to solve the problem but l need to do more leet code questions

--- assessment.py
+++ assessment.py
@@ -1,12 +1,14 @@
-text = input("Enter text: ")
-
-x = text.lower().split()
+text = (input("Enter text: "))
 
 if len(text) < 1 or len(text) > 1000:
     print("your text is too short/too long")
     text = input("Enter text: ")
 else:
-    print("Input accepted.")
+    print("Input accepted")
 
+def palindrome(text):
+    for ch in text:
+        x = [].append()
+        print(x)
 
- 
+#l have to do more leet code questions cause l really dont know how to solve this!!
